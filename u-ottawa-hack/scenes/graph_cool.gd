68622f8162c5c816f196d8f4e174bfe80aa35381
BradyParisien <bradyparisien37@gmail.com>
2026-01-17 21:13:07 -0500

    cleaned graph

--- u-ottawa-hack/scenes/graph_cool.gd
+++ u-ottawa-hack/scenes/graph_cool.gd
@@ -6,16 +6,23 @@ var horz_dist : float
 var vert_dist : float
 
 func _ready() -> void:
-	var botRight = get_node("ColorRect2/Node2D3")
+	var botRight : Node2D = get_node("ColorRect2/Node2D3")
 	
-	horz_dist = abs(botRight.x)
-	vert_dist = abs(botRight.y)
+	horz_dist = abs(botRight.position.x)
+	vert_dist = abs(botRight.position.y)
 
 func makeGraph(values: Array[float], metric_name_: String):
 	metric_name.text = metric_name_
 	var step_size = horz_dist/len(values)
 	var line : Line2D = Line2D.new()
-	for i in range(0,len(values)):
-		line.add_point(Vector2(step_size*i,values[i]))
+	var highest : float = -9999999
 	
-	add_child(line)
+	for i in values:
+		if i > highest:
+			highest = i
+	
+	
+	for i in range(0,len(values)):
+		line.add_point(Vector2(step_size*i,(values[i]/highest)*vert_dist))
+	
+	get_node("ColorRect2").add_child(line)

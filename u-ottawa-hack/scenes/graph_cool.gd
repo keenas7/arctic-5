3753f4167019d8f4fa92ca68ee2fb3dd0f2acc16
BradyParisien <bradyparisien37@gmail.com>
2026-01-17 20:57:49 -0500

    finished graph

--- u-ottawa-hack/scenes/graph_cool.gd
+++ u-ottawa-hack/scenes/graph_cool.gd
@@ -2,6 +2,20 @@ extends Control
 class_name GraphCool
 
 @export var metric_name : Label
+var horz_dist : float
+var vert_dist : float
+
+func _ready() -> void:
+	var botRight = get_node("ColorRect2/Node2D3")
+	
+	horz_dist = abs(botRight.x)
+	vert_dist = abs(botRight.y)
 
 func makeGraph(values: Array[float], metric_name_: String):
-	pass
+	metric_name.text = metric_name_
+	var step_size = horz_dist/len(values)
+	var line : Line2D = Line2D.new()
+	for i in range(0,len(values)):
+		line.add_point(Vector2(step_size*i,values[i]))
+	
+	add_child(line)
